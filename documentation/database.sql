4521cedd7204eb5fb5f9628e742b70bd8e639de4
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-07-17 03:43:02 +0200

    added db script

---
+++ documentation/database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE standing_desk;
+
+USE standing_desk;
+
+CREATE TABLE desk (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    height INT,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
