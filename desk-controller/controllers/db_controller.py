09204a02a580c75e7739ab3ed30300c06c8921b8
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-07-29 21:39:46 +0200

    save height to db

--- desk-controller/controllers/db_controller.py
+++ desk-controller/controllers/db_controller.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 
 class DatabaseController:
-    INSERT_HEIGHT_QUERRY = ""
+    INSERT_HEIGHT_QUERRY = "INSERT INTO desk (height) VALUES ({});"
     GET_ALL_HEIGHTS_QUERRY = ""
     GET_TODAYS_TOTAL_TIME_QUERRY = ""
     GET_AVG_TOTAL_TIME_QUERRY = ""
@@ -12,22 +12,22 @@ class DatabaseController:
     def __init__(self) -> None:
         load_dotenv()
         self.user = os.getenv("DB_USER")
-        self.password=os.getenv("DB_PASSWORD")
-        self.host=os.getenv("DB_HOST")        
-        self.port=int(os.getenv("DB_PORT", 3306))
-        self.database=os.getenv("DB_NAME")
-        
-        print(self.user)
-        
+        self.password = os.getenv("DB_PASSWORD")
+        self.host = os.getenv("DB_HOST")
+        self.port = int(os.getenv("DB_PORT", 3306))
+        self.database = os.getenv("DB_NAME")
+
+        if not self.user:
+            raise ValueError("Database configuration not found")
 
     def connect(self):
         try:
             self.conn = mariadb.connect(
-                user= self.user,
-                password= self.password,
-                host= self.host,
-                port= self.port,
-                database= self.database,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port,
+                database=self.database,
             )
         except Exception as e:
             print(f"Failed to connect to database {e}")
@@ -36,7 +36,13 @@ class DatabaseController:
         self.conn.close()
 
     def save_height(self, height):
-        return self.port
-        pass
+        self.connect()
+        cursor = self.conn.cursor()
+        cursor.execute(self.INSERT_HEIGHT_QUERRY.format(height))
+        self.conn.commit()
+        self.close()
+
+        return
+
 
 db_controller = DatabaseController()
