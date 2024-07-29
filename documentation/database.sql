a650f190a84674dff143b7e1e3c30f6d0757fe12
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-07-29 20:52:14 +0200

    added db user

--- documentation/database.sql
+++ documentation/database.sql
@@ -1,3 +1,4 @@
+CREATE USER 'deskController'@'localhost' IDENTIFIED BY 'password';
 CREATE DATABASE standing_desk;
 
 USE standing_desk;
@@ -5,5 +6,7 @@ USE standing_desk;
 CREATE TABLE desk (
     id INT AUTO_INCREMENT PRIMARY KEY,
     height INT,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
 );
+
+GRANT INSERT, SELECT ON standing_desk.desk TO 'deskController'@'localhost';
