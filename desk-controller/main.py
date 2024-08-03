d050e58314b23dd8055cba6597e5224463308dad
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-08-03 20:09:04 +0200

    switched flask to bottle

--- desk-controller/main.py
+++ desk-controller/main.py
@@ -1,20 +1,19 @@
 import signal
 import sys
-from flask import Flask
 from threading import Thread, Event
 import time
-from routes.desk_routes import desk_bp
+from bottle import Bottle
 from utils.desk_state import desk_state
 from utils.gpio_service import gpio_service
 from controllers.db_controller import db_controller
 from controllers.desk_controller import desk_controller
-
-app = Flask(__name__)
-app.register_blueprint(desk_bp, url_prefix="/api")
+from routes.height_routes import height_server
+from routes.setting_routes import setting_server
+from routes.time_routes import time_server
 
 # Create a shutdown event to signal the background thread to stop
 shutdown_event = Event()
-
+mainApp = Bottle()
 
 def exit():
     """
@@ -34,13 +33,12 @@ def get_current_height_loop():
     print("To get the latest state of the Desk please press the move up/down button")
     while not shutdown_event.is_set():
         try:
-            time.sleep(1)  # Change height every 10 seconds
-            with app.app_context():  # Access the api context
-                desk_controller.measure_desk_height()
+            time.sleep(0.5)  # Change height every 10 seconds
+            desk_controller.measure_desk_height()
 
-                if desk_controller.height_has_changed():
-                    db_controller.save_height(desk_state.get_height())
-                    desk_controller.reset_height_has_changed()
+            if desk_controller.height_has_changed():
+                db_controller.save_height(desk_state.get_height())
+                desk_controller.reset_height_has_changed()
         except Exception as e:  
             print(f"Error in get_current_height_loop: {e}")
 
@@ -52,11 +50,10 @@ def change_desk_height_loop():
     print("Started change height loop")
     while not shutdown_event.is_set():
         try:
-            with app.app_context():
-                time.sleep(5)
-                if desk_state.should_desk_be_moved():
-                    print("moving desk ...")
-                    desk_controller.move(desk_state.get_moving_direction())
+            time.sleep(5)
+            if desk_state.should_desk_be_moved():
+                print("moving desk ...")
+                desk_controller.move(desk_state.get_moving_direction())
         except Exception as e:
             print(f"Error in change_desk_height_loop: {e}")
 
@@ -84,7 +81,11 @@ if __name__ == "__main__":
 
     # Run the Flask app
     try:
-        app.run(debug=True)
+        mainApp.mount('/height', height_server)
+        mainApp.mount("/time", time_server)
+        mainApp.mount("/setting", setting_server)
+
+        mainApp.run()
     except Exception as e:
         print(f"Error in main block: {e}") 
     finally:
