c0f1cb704b940f7b88e088f26d43dcea38acd8e7
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-08-11 03:54:16 +0200

    reset webinterface and added angular material

--- webinterface/src/app/app.component.ts
+++ webinterface/src/app/app.component.ts
@@ -1,27 +1,15 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TimeHeatmapComponent } from "./time-heatmap/time-heatmap.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, TimeHeatmapComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'webinterface';
-
-  // Constructor with HttpClient injection
-  constructor(private http: HttpClient) {}
-
-  // Updated API method to call an example API and log the response
-  public changeDeskHeight(status: "UP" | "DOWN") {
-    const apiUrl = 'http://192.168.178.122:5000/move/' + status; // Placeholder URL
-    this.http.get(apiUrl).subscribe({
-      next: (response) => console.log(response),
-      error: (error) => console.error('There was an error!', error)
-    });
-  }
 }
