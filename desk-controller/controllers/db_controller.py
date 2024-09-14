e35dc2d146e52a948fab72297e285cc0e4373fb9
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-09-15 00:54:51 +0200

    changed time routes

--- desk-controller/controllers/db_controller.py
+++ desk-controller/controllers/db_controller.py
@@ -7,11 +7,11 @@ class DatabaseController:
     INSERT_HEIGHT_QUERRY = "INSERT INTO desk (height) VALUES ({});"
     GET_ALL_ENTRYS_QUERRY = "SELECT height, time FROM desk;"
     GET_ALL_ENTRYS_OF_DAY_QUERRY = "SELECT height, time FROM desk WHERE DATE(time) = '{}';"
-    GET_TODAYS_TOTAL_TIME_QUERRY = "SELECT height, SUM(duration_seconds) AS total_duration_seconds FROM ( SELECT height, TIMESTAMPDIFF(SECOND, time, LEAD(time) OVER (ORDER BY time)) AS duration_seconds FROM desk WHERE DATE(time) = CURDATE()) AS durations GROUP BY height;"
     GET_AVG_TOTAL_TIME_QUERRY = "SELECT height, AVG(total_duration_seconds) AS avg_daily_duration_seconds FROM (SELECT height, date, SUM(duration_seconds) AS total_duration_seconds FROM (SELECT height, DATE(time) AS date, TIMESTAMPDIFF(SECOND, time, COALESCE(LEAD(time) OVER (PARTITION BY DATE(time) ORDER BY time), DATE_ADD(DATE(time), INTERVAL 1 DAY))) AS duration_seconds FROM desk) AS daily_durations GROUP BY height, date) AS daily_totals GROUP BY height;"
     GET_SPECIFIC_AVG_TOTAL_TIME_QUERRY = "SELECT AVG(total_duration_seconds) AS avg_daily_duration_seconds FROM (SELECT height, date, SUM(duration_seconds) AS total_duration_seconds FROM (SELECT height, DATE(time) AS date, TIMESTAMPDIFF(SECOND, time, COALESCE(LEAD(time) OVER (PARTITION BY DATE(time) ORDER BY time), DATE_ADD(DATE(time), INTERVAL 1 DAY))) AS duration_seconds FROM desk) AS daily_durations GROUP BY height, date) AS daily_totals WHERE height={} GROUP BY height;"
     GET_TOTAL_TIMES_QUERRY = "SELECT height, SUM(duration_seconds) AS total_duration_seconds FROM ( SELECT height, TIMESTAMPDIFF(SECOND, time, LEAD(time) OVER (ORDER BY time)) AS duration_seconds FROM desk) AS durations GROUP BY height;"
-
+    GET_ACTIVITY_QUERRY = "WITH desk_with_lead AS (SELECT id, height, time,LEAD(time) OVER (ORDER BY time) AS next_time FROM desk) SELECT DATE(time) AS day, height, SEC_TO_TIME(SUM(TIMESTAMPDIFF(SECOND, time, COALESCE(next_time, NOW())))) AS total_time FROM desk_with_lead WHERE height={} GROUP BY day, height ORDER BY day;"
+    
     def __init__(self) -> None:
         load_dotenv()
         self.user = os.getenv("DB_USER")
@@ -65,19 +65,21 @@ class DatabaseController:
         finally:
             self.close()
             
-    def get_todays_total(self):
+    def get_todays_total(self, height):
+        query = "SELECT height, SUM(duration_seconds) AS total_duration_seconds FROM ( SELECT height, TIMESTAMPDIFF(SECOND, time, LEAD(time) OVER (ORDER BY time)) AS duration_seconds FROM desk WHERE DATE(time) = CURDATE()) AS durations WHERE height={} GROUP BY height;"
+        
         try:
             self.connect()
             cursor = self.conn.cursor()
-            cursor.execute(self.GET_TODAYS_TOTAL_TIME_QUERRY)
+            cursor.execute(query.format(height))
             rows = []
 
             for (height, total_duration_seconds) in cursor:
                 total_duration_minutes = round(total_duration_seconds / 60)
-                rows.append({"height" : height, "total_duration_seconds" : str(total_duration_minutes)})
-            return str(rows)
+                rows.append({"height" : height, "total_time" : str(total_duration_minutes)})
+            return json.dumps(rows)
         except Exception as e:
-            return str(e)
+            return json.dumps({"error": str(e)})
         finally:
             self.close()
     
@@ -91,11 +93,44 @@ class DatabaseController:
             for(height, time) in cursor:
                 rows.append({"height": str(height), "time" : str(time)})
 
-            return str(rows)
+            return json.dumps(rows)
         except Exception as e:
-            return str(e)
+            return json.dumps({"error": str(e)})
         finally:
             self.close()
 
+    def get_daily_activity(self, height):
+        try:
+            self.connect()
+            cursor = self.conn.cursor()
+            print(self.GET_ALL_ENTRYS_QUERRY.format(height))
+            cursor.execute(self.GET_ACTIVITY_QUERRY.format(height))
+            rows = []
+            
+            for(day, height, total_time) in cursor:
+                rows.append({"day": str(day), "total_time" : str(total_time)})
 
+            return json.dumps(rows)
+        except Exception as e:
+            return json.dumps({"error": str(e)})
+        finally:
+            self.close()
+            
+    def get_yesterdays_total(self, height):
+        query = "SELECT height, SUM(duration_seconds) AS total_duration_seconds FROM ( SELECT height, TIMESTAMPDIFF(SECOND, time, LEAD(time) OVER (ORDER BY time)) AS duration_seconds FROM desk WHERE DATE(time) = CURDATE() - INTERVAL 1 DAY) AS durations WHERE height={} GROUP BY height;"
+
+        try:
+            self.connect()
+            cursor = self.conn.cursor()
+            cursor.execute(query.format(height))
+            rows = []
+
+            for (height, total_duration_seconds) in cursor:
+                total_duration_minutes = round(total_duration_seconds / 60)
+                rows.append({"height" : height, "total_time" : str(total_duration_minutes)})
+            return json.dumps(rows)
+        except Exception as e:
+            return json.dumps({"error": str(e)})
+        finally:
+            self.close()
 db_controller = DatabaseController()
--- desk-controller/routes/time_routes.py
+++ desk-controller/routes/time_routes.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, abort
+from bottle import Bottle, abort, response
 from controllers.db_controller import db_controller
 time_server = Bottle()
 
@@ -13,5 +13,15 @@ def get_percentage_standing_time():
      
 @time_server.route('/today')
 def get_todays_total_time():
-    return db_controller.get_todays_total()
+    response.headers['Content-type'] = 'application/json'
+    return db_controller.get_todays_total(115)
 
+@time_server.route('/yesterday')
+def get_todays_total_time():
+    response.headers['Content-type'] = 'application/json'
+    return db_controller.get_yesterdays_total(115)
+
+@time_server.route('/activity/<height>')
+def get_daily_activity(height):
+    response.headers['Content-type'] = 'application/json'
+    return db_controller.get_daily_activity(height)
