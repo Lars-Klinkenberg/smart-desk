6b1f52f064526a3cfba99e85cd31ab3ccafe1b49
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-09-16 21:46:31 +0200

    new db

--- documentation/database.sql
+++ documentation/database.sql
@@ -1,12 +1,39 @@
-CREATE USER 'deskController'@'localhost' IDENTIFIED BY 'password';
+DROP DATABASE IF EXISTS standing_desk;
 CREATE DATABASE standing_desk;
 
-USE standing_desk;
 
-CREATE TABLE desk (
+USE standing_desk;
+CREATE USER 'deskController'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON standing_desk.* to "deskController"@"localhost";
+
+CREATE TABLE heights(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    height INT,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    start_height INT,
+    end_time TIMESTAMP,
+    end_height INT
 );
 
-GRANT INSERT, SELECT ON standing_desk.desk TO 'deskController'@'localhost';
+CREATE TABLE daily_avg(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    height INT,
+    total_time TIME,
+    day TIMESTAMP
+);
+
+CREATE TABLE monthly_avg(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    height INT,
+    total_time TIME,
+    id_of_month ENUM("1","2","3","4","5","6","7","8","9","10","11","12"),
+    year INT
+);
+
+DELIMITER //
+
+CREATE PROCEDURE getAllHeights()
+BEGIN
+	SELECT * FROM your_table ORDER BY id DESC LIMIT 1;
+END //
+
+DELIMITER ;
