88fb023e541049113f3e6bc37a67642fbbdf62db
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-09-18 21:53:28 +0200

    added return type json to all requests

--- api/utils/enable_cors.py
+++ api/utils/enable_cors.py
@@ -1,26 +1,31 @@
 import bottle
 from bottle import response
 
+
 class EnableCors(object):
-    name = 'enable_cors'
+    name = "enable_cors"
     api = 2
 
     def apply(self, fn, context):
         def _enable_cors(*args, **kwargs):
             # set CORS headers
-            response.headers['Access-Control-Allow-Origin'] = '*'
-            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+            response.headers["Access-Control-Allow-Origin"] = "*"
+            response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, OPTIONS"
+            response.headers["Access-Control-Allow-Headers"] = (
+                "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+            )
 
-            if bottle.request.method != 'OPTIONS':
+            if bottle.request.method != "OPTIONS":
                 # actual request; reply with the actual response
                 return fn(*args, **kwargs)
 
         return _enable_cors
 
+
 def add_cors_headers():
-    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
-    bottle.response.headers['Access-Control-Allow-Methods'] = \
-        'GET, POST, PUT, OPTIONS'
-    bottle.response.headers['Access-Control-Allow-Headers'] = \
-        'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+    bottle.response.headers["Access-Control-Allow-Origin"] = "*"
+    bottle.response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, OPTIONS"
+    bottle.response.headers["Access-Control-Allow-Headers"] = (
+        "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+    )
+    bottle.response.headers["Content-type"] = "application/json"
