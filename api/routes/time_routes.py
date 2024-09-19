5ae849512d7abf60c24f0aeb9f2ba0712b5ecf51
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-09-20 00:43:50 +0200

    added year route

--- api/routes/time_routes.py
+++ api/routes/time_routes.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, request
+from bottle import Bottle, request, abort
 from controllers.db_controller import db_controller
 
 time_server = Bottle()
@@ -13,6 +13,14 @@ def get_todays_total_time():
 def get_todays_total_time():
     return db_controller.get_yesterdays_total()
 
+@time_server.route("/year")
+def get_todays_total_time():
+    year = request.headers.get("year", None)
+    print(year)
+    if year is None:
+        abort(400)
+    
+    return db_controller.get_daily_totals_of_year(year)
 
 @time_server.route("/day")
 def get_todays_total_time():
