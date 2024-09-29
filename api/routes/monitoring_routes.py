01860822be152b3ce8059f2280b401a77060a82a
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-09-29 22:53:11 +0200

    added api routes for all logs

--- api/routes/monitoring_routes.py
+++ api/routes/monitoring_routes.py
@@ -1,7 +1,28 @@
 import json
-from bottle import Bottle, HTTPResponse, request
+from bottle import Bottle
 
 monitoring_server = Bottle()
+
+
+def read_logs(path):
+    logs = []
+    try:
+        with open(path, "r") as log_file:
+            lines = log_file.readlines()
+
+            for line in lines:
+                args = line.split("|")
+
+                if len(args) == 3:
+                    logs.append(
+                        {
+                            "time": args[0].strip(),
+                            "level": args[1].strip(),
+                            "message": args[2].strip(),
+                        }
+                    )
+    finally:
+        return logs
 
 
 @monitoring_server.route("/status")
@@ -12,7 +33,6 @@ def get_status():
     Returns:
         string: serialized json [{"service" : "status"}]
     """
-
     return json.dumps([{"api": "up"}])
 
 
@@ -24,23 +44,7 @@ def get_controller_logs():
     Returns:
         string: serialized json
     """
-    logs = []
-    with open("../logs/api.log", "r") as log_file:
-        lines = log_file.readlines()
-
-        for line in lines:
-            args = line.split("|")
-
-            if len(args) == 3:
-                logs.append(
-                    {
-                        "time": args[0].strip(),
-                        "level": args[1].strip(),
-                        "message": args[2].strip(),
-                    }
-                )
-
-    return json.dumps(logs)
+    return json.dumps(read_logs("../logs/api.log"))
 
 
 @monitoring_server.route("/logs/controller")
@@ -51,20 +55,26 @@ def get_controller_logs():
     Returns:
         string: serialized json
     """
-    logs = []
-    with open("../logs/desk_controller.log", "r") as log_file:
-        lines = log_file.readlines()
+    return json.dumps(read_logs("../logs/desk_controller.log"))
 
-        for line in lines:
-            args = line.split("|")
 
-            if len(args) == 3:
-                logs.append(
-                    {
-                        "time": args[0].strip(),
-                        "level": args[1].strip(),
-                        "message": args[2].strip(),
-                    }
-                )
+@monitoring_server.route("/logs/jobs/daily")
+def get_controller_logs():
+    """
+    returns the logs of the daily job
 
-    return json.dumps(logs)
+    Returns:
+        string: serialized json
+    """
+    return json.dumps(read_logs("../logs/calculate_daily_activity.log"))
+
+
+@monitoring_server.route("/logs/jobs/monthly")
+def get_controller_logs():
+    """
+    returns the logs of the monthly job
+
+    Returns:
+        string: serialized json
+    """
+    return json.dumps(read_logs("../logs/calculate_monthly_activity.log"))
