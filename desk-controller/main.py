aa999348d8524866ef79d04aa847aa54c1f12dad
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-09-29 02:24:49 +0200

    added logger to desk-controller

--- desk-controller/main.py
+++ desk-controller/main.py
@@ -6,6 +6,7 @@ from utils.desk_state import desk_state
 from utils.gpio_service import gpio_service
 from controllers.desk_controller import desk_controller
 from controllers.http_controller import http_controller
+import logging
 
 # Create a shutdown event to signal the background thread to stop
 shutdown_event = Event()
@@ -15,8 +16,8 @@ def get_current_height_loop():
     """
     Background thread to update the desk height
     """
+    logger.info("Started loop with current height " + str(desk_state.get_height()))
 
-    print("Started loop with current height " + str(desk_state.get_height()))
     while not shutdown_event.is_set():
         try:
             time.sleep(0.5)  # Change height every 10 seconds
@@ -25,20 +26,27 @@ def get_current_height_loop():
             if desk_controller.height_has_changed():
                 http_controller.save_height(desk_state.get_height())
                 desk_controller.reset_height_has_changed()
-                print("height has ben changed ...")
+                logger.info("height has ben changed ...")
         except Exception as e:
-            print(f"Error in get_current_height_loop: {e}")
+            logger.error(f"exception while running get_current_height_loop: {e}")
 
 
 def shutdown_handler(signum, frame):
     """
     Signal handler to gracefully shut down the loop.
     """
-    print("Received shutdown signal. Stopping the loop...")
+    logger.info("Received shutdown signal. Stopping the loop...")
     shutdown_event.set()  # Trigger the event to stop the loop
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        filename="../logs/desk_controller.log",
+        encoding="utf-8",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s: %(message)s",
+    )
     signal.signal(signal.SIGINT, shutdown_handler)
     signal.signal(signal.SIGTERM, shutdown_handler)
 
@@ -46,6 +54,6 @@ if __name__ == "__main__":
         desk_state.set_height(http_controller.get_current_height())
         get_current_height_loop()
     except Exception as e:
-        print(f"Error running loop: {e}")
+        logger.error(f"Failed running main loop: {e}")
     finally:
-        print("Stopped")
+        logger.info("Stopped desk_controller loop")
