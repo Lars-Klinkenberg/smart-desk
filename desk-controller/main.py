4c547fdac2c22bad7d54b93ad8eb456b7757704b
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-12-05 21:03:02 +0100

    updated logs in desk-controller

--- desk-controller/main.py
+++ desk-controller/main.py
@@ -1,10 +1,10 @@
 import signal
 from threading import Event
 import time
-import logging
 from utils.desk_state import desk_state
 from controllers.desk_controller import desk_controller
 from controllers.http_controller import http_controller
+from shared.logger import log
 
 # Create a shutdown event to signal the loop to stop
 shutdown_event = Event()
@@ -14,7 +14,7 @@ def get_current_height_loop():
     """
     Background thread to update the desk height
     """
-    logger.info("Started loop with current height " + str(desk_state.get_height()))
+    log("desk_controller", "INFO", "Started loop with current height " + str(desk_state.get_height()))
 
     while not shutdown_event.is_set():
         try:
@@ -24,44 +24,37 @@ def get_current_height_loop():
             if desk_controller.height_has_changed():
                 http_controller.save_height(desk_state.get_height())
                 desk_controller.reset_height_has_changed()
-                logger.info("height has ben changed ...")
+                log("desk_controller", "INFO", "height has ben changed ...")
         except Exception as e:
             if (
                 "returned no data (device disconnected or multiple access on port?)"
                 in str(e)
             ):
-                logger.warning(e)
+                log("desk_controller", "WARNING", e)
             elif "Could not configure port: (5, 'Input/output error')" in str(e):
-                logger.critical("No acces to serial port. shutting down ...")
+                log("desk_controller", "CRITICAL", "No acces to serial port. shutting down ...")
                 shutdown_event.set()
             else:
-                logger.exception("exception while running get_current_height_loop")
+                log("desk_controller", "ERROR", "exception while running get_current_height_loop")
 
 
 def shutdown_handler(signum, frame):
     """
     Signal handler to gracefully shut down the loop.
     """
-    logger.info("Received shutdown signal. Stopping the loop...")
+    log("desk_controller", "INFO", "Received shutdown signal. Stopping the loop...")
     shutdown_event.set()  # Trigger the event to stop the loop
 
 
 if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(
-        filename="../logs/desk_controller.log",
-        encoding="utf-8",
-        level=logging.DEBUG,
-        format="%(asctime)s | %(levelname)s | %(message)s",
-    )
     signal.signal(signal.SIGINT, shutdown_handler)
     signal.signal(signal.SIGTERM, shutdown_handler)
 
     try:
-        logger.info("starting desk_controller ...")
+        log("desk_controller", "INFO", "starting desk_controller ...")
         desk_state.set_height(http_controller.get_current_height())
         get_current_height_loop()
     except Exception:
-        logger.exception("Failed running main loop")
+        log("desk_controller", "ERROR", "Failed running main loop")
     finally:
-        logger.info("Stopped desk_controller loop")
+        log("desk_controller", "INFO", "Stopped desk_controller loop")
